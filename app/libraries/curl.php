5d2533557003d5204f2bb221d76a1859119ff156
Minh Duc Nguyen <dekarvn@gmail.com>
2014-02-06 12:14:57 +1100

    possible fix

--- app/libraries/curl.php
+++ app/libraries/curl.php
@@ -7,6 +7,7 @@ class Curl{
 		curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
 		curl_setopt($curl, CURLOPT_POST, false);
 		curl_setopt($curl, CURLOPT_VERBOSE, true);
+		curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
 		$data = curl_exec($curl);
 		return json_decode($data);
 	}
@@ -18,6 +19,7 @@ class Curl{
 		curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
 		curl_setopt($curl, CURLOPT_POST, false);
 		curl_setopt($curl, CURLOPT_VERBOSE, true);
+		curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
 		$data = curl_exec($curl);
 		return $data;
 	}
